99777e134fcd536259cee727c43864f4f4a65921
Sérgio Bueno <84647000+Coastony@users.noreply.github.com>
2021-10-31 17:51:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+    github: 'Coastony',
+    youtube: 'Coastony',
+    facebook: 'Coastony',
+    instagram: 'Coastony',
+    twitter: 'Coastony'
+}
+
+function changeSocialMediaLinks() {
+    for (let li of changeSocialMediaLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+}
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userBio.textContent = data.bio
+            userLink.href = data.html_url
+            userImage.src = data.avatar_url
+            userLogin.textContent = data.login
+        })
+}
+
+getGitHubProfileInfos()
